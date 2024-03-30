5c27fc5dd998baab8b2f29e952e04c918b90484e
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-03-30 14:40:42 +0300

    0.0.3

--- my-app/src/components/Drawing.js
+++ my-app/src/components/Drawing.js
@@ -1,3 +1,4 @@
+
 function Drawing() {
     const styleCyrc = {
         fill: 'transparent',
@@ -8,27 +9,31 @@ function Drawing() {
     const amountOfPoints1 = 10; // количество точек
     let x1, y1;
     const arr1 = [];
+    let j1 = 0;
     for(let i = 0; i < Math.PI * 2; i += Math.PI * 2 / amountOfPoints1){
+        j1++;
         x1 = 700 + r1 * Math.sin(i);
         y1 = 400 + r1 * Math.cos(i);
-        arr1.push(<circle cx={x1} cy={y1} r={10} style={styleCyrc}  />);
+        arr1.push(<circle className={'littleInCircle' + j1} key={i} cx={x1} cy={y1} r={10} style={styleCyrc}  />);
     }
 
     const r2 = 300; // радиус
     const amountOfPoints2 = 29; // количество точек
     let x2, y2;
     const arr2 = [];
+    let j2 = 0;
     for(let i = 0; i < Math.PI * 2; i += Math.PI * 2 / amountOfPoints2){
+        j2++;
         x2 = 700 + r2 * Math.sin(i);
         y2 = 400 + r2 * Math.cos(i);
-        arr2.push(<circle cx={x2} cy={y2} r={10} style={styleCyrc}  />);
+        arr2.push(<circle className={'littleOutCircle' + j2} key={i} cx={x2} cy={y2} r={10} style={styleCyrc}  />);
     }
 
     return (
         <svg viewBox='0 0 1100 1100' width='1100' heigth='1100'>
-            <circle classID='circle1' cx={700} cy={400} r={150} style={styleCyrc}  />
+            <circle className='circle1' cx={700} cy={400} r={150} style={styleCyrc}  />
             {arr1}
-            <circle classID='circle1' cx={700} cy={400} r={300} style={styleCyrc}  />
+            <circle  className='circle2' cx={700} cy={400} r={300} style={styleCyrc}  />
             {arr2}
         </svg>
     );
