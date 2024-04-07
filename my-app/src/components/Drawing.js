5ca4e0ab69d0960a04150719bf34cf07b842a92a
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-04-07 16:55:14 +0300

    0.0.14

--- my-app/src/components/Drawing.js
+++ my-app/src/components/Drawing.js
@@ -4,7 +4,13 @@ function Drawing({nameSkills}) {
         fill: 'transparent',
         stroke: "grey", 
     }
-
+/*
+    const styleTextIn = {
+        display: 'block',
+        width: '20px',
+        wordWrap: 'break-word',
+    }
+*/
     const r1 = 140; 
     const amountOfPoints1 = 10; 
     let x1, y1;
@@ -18,7 +24,8 @@ function Drawing({nameSkills}) {
         y1 = 400 - r1 * Math.cos(i);
         arr1.push(<>
             <circle className={'littleInCircle' + j1} key={i} cx={x1} cy={y1} r={13} fill='grey' fillOpacity='0.7' />
-            <text className={'textOut' + j1} x={700 - 250 * Math.sin(i) + 45 * Math.sin(i) - 60} y={400 - 250 * Math.cos(i) + 45 * Math.cos(i)}>{nameSkills[k1].name}</text>
+            <text className={'textOut' + j1} x={700 - 250 * Math.sin(i) + 45 * Math.sin(i) - 60}
+             y={400 - 250 * Math.cos(i) + 45 * Math.cos(i)} /*style={styleTextIn}*/>{nameSkills[k1].name}</text>
         </>);
     }
 
@@ -27,13 +34,16 @@ function Drawing({nameSkills}) {
     let x2, y2;
     const arr2 = [];
     let j2 = 0;
-    for(let i = 0; i < Math.PI * 2; i += Math.PI * 2 / amountOfPoints2) {
+    let k2 = -1;
+    for(let i = Math.PI * 2; i > 0; i -= Math.PI * 2 / amountOfPoints2) {
         j2++;
-        x2 = 700 + r2 * Math.sin(i);
-        y2 = 400 + r2 * Math.cos(i);
+        k2++;
+        x2 = 700 - r2 * Math.sin(i);
+        y2 = 400 - r2 * Math.cos(i);
         arr2.push(<>
             <circle className={'littleOutCircle' + j2} key={i} cx={x2} cy={y2} r={13} fill='orange' fillOpacity='0.6' />
-            <text className={'textOut' + j2} x={x2 + 50 * Math.sin(i)} y={y2 + 50 * Math.cos(i)}>text</text>    
+            <text className={'textOut' + j2} x={700 - 380 * Math.sin(i) + 50 * Math.sin(i) - 17} 
+             y={400 - 380 * Math.cos(i) + 50 * Math.cos(i)}>{'text' + k2}</text>    
         </>
         );
     }
